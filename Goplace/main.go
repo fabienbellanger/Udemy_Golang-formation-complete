121303d82722f0d9014adb945b4e86f5e9f22aee
Fabien Bellanger <valentil@gmail.com>
2019-10-28 18:12:38 +0100

    Mise en place du cadre

--- Goplace/main.go
+++ Goplace/main.go
@@ -1,6 +1,24 @@
+// Udemy - Goplace project
+//
+// Result:
+// == Summary ==
+// Number of occurrences of Go: 10
+// Number of lines: 7
+// Lines: [ 1 - 8 - 15 - 17 - 19 - 23 - 28 ]
+// == End of Summary ==
+
 package main
 
 func main() {
 
 }
 
+// FindReplaceFile function
+func FindReplaceFile(src, old, new string) (occ int, lines []int, err error) {
+	return
+}
+
+// ProcessLine function
+func ProcessLine(line, old, new string) (found bool, res string, occ int) {
+	return
+}
